0fc98c2d74443ebc267f20f5d5d78c68a1321062
fawn <fawn@rinici.de>
2024-10-15 08:11:10 +0300

    △

--- src/wm.zig
+++ src/wm.zig
@@ -42,7 +42,11 @@ pub const WM = struct {
         log.info("fire walk with me", .{});
 
         _ = c.XSetErrorHandler(Self.on_error);
-        _ = c.XSelectInput(self.x_display, self.x_root, c.StructureNotifyMask | c.SubstructureRedirectMask | c.SubstructureNotifyMask | c.ButtonPressMask | c.PointerMotionMask);
+        _ = c.XSelectInput(self.x_display, self.x_root, c.StructureNotifyMask |
+            c.SubstructureRedirectMask |
+            c.SubstructureNotifyMask |
+            c.ButtonPressMask |
+            c.PointerMotionMask);
         _ = c.XDefineCursor(self.x_display, self.x_root, C.normal);
 
         _ = c.XSync(self.x_display, c.False);
@@ -82,7 +86,12 @@ pub const WM = struct {
             else => {
                 var error_text: [1024]u8 = undefined;
                 _ = c.XGetErrorText(display, event.error_code, &error_text, 1024);
-                log.err("Received X error:\n    Request: {d}\n    Error code: {s} ({d})\n    Resource ID: {d}", .{ event.request_code, error_text, event.error_code, event.resourceid });
+                log.err(
+                    \\Received X error:
+                    \\    Request: {d}
+                    \\    Error code: {s} ({d})
+                    \\    Resource ID: {d}
+                , .{ event.request_code, error_text, event.error_code, event.resourceid });
             },
         }
 
